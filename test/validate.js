14e49c4eae4e711addba59683b4589df630ac0c2
jopemachine <jopemachine@naver.com>
2021-06-16 17:30:06 +0900

    Update schemas

--- test/validate.js
+++ test/validate.js
@@ -2,15 +2,26 @@ import test from "ava";
 import { validate } from "../dist/validator";
 import fse from "fs-extra";
 
-test("validate test - must be true", async t => {
+test("workflow validate test 1 - must be true", async t => {
   const jsonData = await fse.readJson("./test/mock-workflow.json");
   const { valid } = validate(jsonData, "workflow");
   t.is(valid, true);
 });
 
-test("validate test - must be false", async t => {
-    const jsonData = await fse.readJson("./test/mock-invalid-workflow.json");
-    const { valid } = validate(jsonData, "workflow");
-    t.is(valid, false);
-  });
-  
+// test("workflow validate test 2 - must be false", async t => {
+//   const jsonData = await fse.readJson("./test/invalid-mock-workflow.json");
+//   const { valid } = validate(jsonData, "workflow");
+//   t.is(valid, false);
+// });
+
+// test("plugin validate test 1 - must be true", async t => {
+//   const jsonData = await fse.readJson("./test/mock-plugin.json");
+//   const { valid } = validate(jsonData, "workflow");
+//   t.is(valid, true);
+// });
+
+// test("plugin validate test 2 - must be false", async t => {
+//   const jsonData = await fse.readJson("./test/invalid-mock-plugin.json");
+//   const { valid } = validate(jsonData, "workflow");
+//   t.is(valid, false);
+// });
